5a760ea97df19e8d35674284281c30b1f6a92d0d
Mohamed-Youssef Sbita <msbita@paul-f3Ar5s7.clusters.42paris.fr>
2025-04-29 20:15:42 +0200

    Added header to fils of gnl

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msbita <msbita@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 19:13:19 by msbita            #+#    #+#             */
+/*   Updated: 2025/04/29 19:13:19 by msbita           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -10,25 +22,16 @@
 #  define BUFFER_SIZE 42
 # endif
 
-// Limite possible pour le nombre de FDs gérés simultanément.
-// Utiliser OPEN_MAX est idéal mais peut être non portable ou nécessiter des flags.
-// Une valeur fixe comme 1024 est une alternative sûre.
 # ifndef OPEN_MAX
 #  define OPEN_MAX 1024
 # endif
 
-// Pas de définition de structure ici si on utilise une variable statique
-// entièrement gérée dans le .c
-
-// --- Prototypes des fonctions Utilitaires Bonus ---
-// (Ce sont les mêmes que pour la partie obligatoire)
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char *s1, const char *s2);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 
-// --- Prototype de la fonction GNL Bonus ---
 char	*get_next_line_bonus(int fd);
 
 #endif
